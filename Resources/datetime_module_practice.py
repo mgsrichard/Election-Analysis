f850e59c7e7aa0517e1c905cf25961e68cbf2e60
Martha Richardson <mgsrichard@yahoo.com>
2021-12-09 13:34:42 -0500

    Files for week 3 challenge submission

---
+++ Resources/datetime_module_practice.py
@@ -0,0 +1,11 @@
+# Import the datetime class from the datetime module
+
+import datetime as dt
+
+# Use the now() attribute on the datetime class to ge the present time
+
+now = dt.datetime.now()
+
+# Print the present time
+
+print("The time right now is ",now)
